5a308c3304c2d2fa603f68ab3b6bf10ef92f4413
Luis Castaneda <lcastanedad@deloitte.com>
2023-08-17 15:00:11 -0600

    Agrego estado

---
+++ .sfdx/typings/lwc/apex/ControladorEncuesta.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/ControladorEncuesta.getCaso" {
+  export default function getCaso(param: {recordId: any}): Promise<any>;
+}
+declare module "@salesforce/apex/ControladorEncuesta.actualizarCaso" {
+  export default function actualizarCaso(param: {jsonCaso: any}): Promise<any>;
+}
