46b6a8e97222d30eba2e7f6a7537f414c0d9c75f
Luis Castaneda <lcastanedad@deloitte.com>
2023-09-22 16:28:28 -0600

    Cambios Perfil

--- force-app/main/default/lwc/fs_Inicio/fs_Inicio.js
+++ force-app/main/default/lwc/fs_Inicio/fs_Inicio.js
@@ -8,6 +8,7 @@ import urlVideo3 from '@salesforce/label/c.FS_UrlVideoDestacado3';
 import tituloVideo3 from '@salesforce/label/c.FS_TituloVideoDestacado3';
 import urlVideo4 from '@salesforce/label/c.FS_UrlVideoDestacado4';
 import tituloVideo4 from '@salesforce/label/c.FS_TituloVideoDestacado4';
+import urlPaginaNoticias from '@salesforce/label/c.FS_UrlPaginaNoticias';
 
 export default class Fs_Inicio extends LightningElement {
     data = {
@@ -18,6 +19,7 @@ export default class Fs_Inicio extends LightningElement {
         tituloVideo1: tituloVideo1,
         tituloVideo2: tituloVideo2,
         tituloVideo3: tituloVideo3,
-        tituloVideo4: tituloVideo4
+        tituloVideo4: tituloVideo4,
+        urlPaginaNoticias: urlPaginaNoticias
     }
 }
